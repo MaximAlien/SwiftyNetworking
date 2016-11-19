b2b3e87606776c9c4379ff13bf8d2af0a4983539
MaximAlien <maxim.makhun@gmail.com>
2016-11-19 18:27:39 +0200

    Add new file for URL cache tests.

---
+++ SwiftyNetworkingTests/URLCacheManagerTests.swift
@@ -0,0 +1,24 @@
+//
+//  URLCacheManagerTests.swift
+//  SwiftyNetworking
+//
+//  Created by Maxim Makhun on 11/19/16.
+//  Copyright © 2016 Maxim Makhun. All rights reserved.
+//
+
+import XCTest
+
+class URLCacheManagerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testThat_profileRequest_shouldHandleCacheReuse() {
+
+    }
+}
