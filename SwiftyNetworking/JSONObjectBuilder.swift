2901ee28a301454a470648459440050aa8cddec8
MaximAlien <maxim.makhun@gmail.com>
2016-12-07 16:49:30 +0200

    Add class and test case for JSON object builder.

---
+++ SwiftyNetworking/JSONObjectBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  JSONObjectBuilder.swift
+//  SwiftyNetworking
+//
+//  Created by Maxim Makhun on 12/7/16.
+//  Copyright © 2016 Maxim Makhun. All rights reserved.
+//
+
+import Cocoa
+
+class JSONObjectBuilder: NSObject {
+
+}
---
+++ SwiftyNetworkingTests/JSONObjectBuilderTests.swift
@@ -0,0 +1,20 @@
+//
+//  JSONObjectBuilderTests.swift
+//  SwiftyNetworking
+//
+//  Created by Maxim Makhun on 12/7/16.
+//  Copyright © 2016 Maxim Makhun. All rights reserved.
+//
+
+import XCTest
+
+class JSONObjectBuilderTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
